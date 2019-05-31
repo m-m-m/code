18a3645352ce228e25021f69fec6dfb2af660375
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-05-31 10:17:44 +0200

    Test to check formatting

--- java/impl/src/main/java/net/sf/mmm/code/impl/java/JavaContext.java
+++ java/impl/src/main/java/net/sf/mmm/code/impl/java/JavaContext.java
@@ -47,217 +47,215 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class JavaContext extends AbstractBaseContextWithCache {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JavaContext.class);
+  private static final Logger LOG = LoggerFactory.getLogger(JavaContext.class);
+
+  /**
+   * The constructor.
+   *
+   * @param source the top-level {@link #getSource() source}.
+   */
+  protected JavaContext(BaseSourceImpl source) {
+
+    super(source);
+  }
+
+  /**
+   * The constructor.
+   *
+   * @param source the top-level {@link #getSource() source}.
+   * @param sourceProvider the {@link BaseSourceProvider}.
+   */
+  public JavaContext(BaseSourceImpl source, BaseSourceProvider sourceProvider) {
+
+    super(source, sourceProvider);
+  }
+
+  /**
+   * @return the root {@link JavaContext context} responsible for the fundamental code (from JDK).
+   */
+  @Override
+  public abstract JavaRootContext getRootContext();
+
+  @Override
+  protected BaseType getTypeFromCache(String qualifiedName) { // make visible
+
+    return super.getTypeFromCache(qualifiedName);
+  }
+
+  @Override
+  public BaseGenericType getType(Type type, CodeElementWithDeclaringType declaringElement) {
+
+    if (type instanceof Class) {
+      return getType((Class<?>) type);
+    } else if (type instanceof ParameterizedType) {
+      ParameterizedType parameterizedType = (ParameterizedType) type;
+      return new BaseParameterizedType((BaseElement) declaringElement, parameterizedType);
+    } else if (type instanceof TypeVariable) {
+      TypeVariable<?> typeVar = (TypeVariable<?>) type;
+      if (declaringElement instanceof BaseType) {
+        BaseType declaringType = (BaseType) declaringElement;
+        assert (typeVar.getGenericDeclaration() == declaringType.getReflectiveObject());
+        return new BaseTypeVariable(declaringType.getTypeParameters(), typeVar);
+      } else if (declaringElement instanceof BaseOperation) {
+        BaseOperation operation = (BaseOperation) declaringElement;
+        if (typeVar.getGenericDeclaration() == operation.getReflectiveObject()) {
+          return new BaseTypeVariable(operation.getTypeParameters(), typeVar);
+        }
+      }
+      BaseTypeVariable typeVariable = (BaseTypeVariable) findTypeVariable(declaringElement, typeVar.getName());
+      if (typeVariable != null) {
+        return typeVariable;
+      }
+      LOG.warn("Could not find type variable {} in {}", typeVar, declaringElement);
+      return new BaseTypeVariable((BaseTypeVariables) declaringElement.getDeclaringType().getTypeParameters(), typeVar);
+    } else if (type instanceof WildcardType) {
+      WildcardType wildcard = (WildcardType) type;
+      BaseNodeItem parent;
+      if (declaringElement instanceof BaseParameterizedType) {
+        parent = ((BaseParameterizedType) declaringElement).getTypeParameters();
+      } else {
+        parent = (BaseNodeItem) declaringElement;
+      }
+      return new BaseTypeWildcard(parent, wildcard);
+    } else if (type instanceof GenericArrayType) {
+      return new BaseArrayType((BaseElementWithDeclaringType) declaringElement, type);
+    } else {
+      throw new IllegalCaseException(type.getClass().getSimpleName());
+    }
+  }
+
+  private static CodeTypeVariable findTypeVariable(CodeNode node, String name) {
+
+    CodeElementWithTypeVariables elementWithTypeVariables = findElementWithTypeVariables(node);
+    if (elementWithTypeVariables != null) {
+      return elementWithTypeVariables.getTypeParameters().get(name, true);
+    }
+    return null;
+  }
+
+  private static CodeElementWithTypeVariables findElementWithTypeVariables(CodeNode node) {
+
+    if (node instanceof CodeElementWithTypeVariables) {
+      return (CodeElementWithTypeVariables) node;
+    } else if (node instanceof BaseOperationArg) {
+      BaseOperationArg arg = (BaseOperationArg) node;
+      BaseOperation operation = arg.getDeclaringOperation();
+      if (operation != null) {
+        return operation;
+      } else {
+        return arg.getDeclaringType();
+      }
+    } else if (node instanceof BaseGenericType) {
+      return findElementWithTypeVariables(node.getParent());
+    } else if (node instanceof BaseTypeVariables) {
+      return findElementWithTypeVariables(node.getParent());
+    } else {
+      throw new IllegalCaseException(node.getClass().getSimpleName());
+    }
+  }
+
+  /**
+   * Implementation of {@link BaseLoader} to load classes from byte-code.
+   *
+   * @see JavaContext#getLoader()
+   */
+  protected class JavaClassLoader implements BaseLoader {
+
+    private final ClassLoader classloader;
+
+    /**
+     * The constructor.
+     */
+    public JavaClassLoader() {
+
+      this(Thread.currentThread().getContextClassLoader());
+    }
 
     /**
      * The constructor.
      *
-     * @param source
-     *            the top-level {@link #getSource() source}.
+     * @param classloader the explicit {@link ClassLoader} to use.
      */
-    protected JavaContext(BaseSourceImpl source) {
+    public JavaClassLoader(ClassLoader classloader) {
 
-        super(source);
-    }
-
-    /**
-     * The constructor.
-     *
-     * @param source
-     *            the top-level {@link #getSource() source}.
-     * @param sourceProvider
-     *            the {@link BaseSourceProvider}.
-     */
-    public JavaContext(BaseSourceImpl source, BaseSourceProvider sourceProvider) {
-
-        super(source, sourceProvider);
-    }
-
-    /**
-     * @return the root {@link JavaContext context} responsible for the fundamental code (from JDK).
-     */
-    @Override
-    public abstract JavaRootContext getRootContext();
-
-    @Override
-    protected BaseType getTypeFromCache(String qualifiedName) { // make visible
-
-        return super.getTypeFromCache(qualifiedName);
+      super();
+      this.classloader = classloader;
     }
 
     @Override
-    public BaseGenericType getType(Type type, CodeElementWithDeclaringType declaringElement) {
+    public BaseSource getSource() {
 
-        if (type instanceof Class) {
-            return getType((Class<?>) type);
-        } else if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-            return new BaseParameterizedType((BaseElement) declaringElement, parameterizedType);
-        } else if (type instanceof TypeVariable) {
-            TypeVariable<?> typeVar = (TypeVariable<?>) type;
-            if (declaringElement instanceof BaseType) {
-                BaseType declaringType = (BaseType) declaringElement;
-                assert (typeVar.getGenericDeclaration() == declaringType.getReflectiveObject());
-                return new BaseTypeVariable(declaringType.getTypeParameters(), typeVar);
-            } else if (declaringElement instanceof BaseOperation) {
-                BaseOperation operation = (BaseOperation) declaringElement;
-                if (typeVar.getGenericDeclaration() == operation.getReflectiveObject()) {
-                    return new BaseTypeVariable(operation.getTypeParameters(), typeVar);
-                }
-            }
-            BaseTypeVariable typeVariable = (BaseTypeVariable) findTypeVariable(declaringElement, typeVar.getName());
-            if (typeVariable != null) {
-                return typeVariable;
-            }
-            LOG.warn("Could not find type variable {} in {}", typeVar, declaringElement);
-            return new BaseTypeVariable((BaseTypeVariables) declaringElement.getDeclaringType().getTypeParameters(),
-                typeVar);
-        } else if (type instanceof WildcardType) {
-            WildcardType wildcard = (WildcardType) type;
-            BaseNodeItem parent;
-            if (declaringElement instanceof BaseParameterizedType) {
-                parent = ((BaseParameterizedType) declaringElement).getTypeParameters();
-            } else {
-                parent = (BaseNodeItem) declaringElement;
-            }
-            return new BaseTypeWildcard(parent, wildcard);
-        } else if (type instanceof GenericArrayType) {
-            return new BaseArrayType((BaseElementWithDeclaringType) declaringElement, type);
-        } else {
-            throw new IllegalCaseException(type.getClass().getSimpleName());
+      return JavaContext.this.getSource();
+    }
+
+    @Override
+    public BaseContext getContext() {
+
+      return JavaContext.this;
+    }
+
+    @Override
+    public BaseType getType(String qualifiedName) {
+
+      Class<?> clazz = null;
+      try {
+        clazz = classloader.loadClass(qualifiedName);
+        if (clazz.isArray()) {
+          throw new IllegalArgumentException(qualifiedName);
         }
-    }
-
-    private static CodeTypeVariable findTypeVariable(CodeNode node, String name) {
-
-        CodeElementWithTypeVariables elementWithTypeVariables = findElementWithTypeVariables(node);
-        if (elementWithTypeVariables != null) {
-            return elementWithTypeVariables.getTypeParameters().get(name, true);
-        }
+        return (BaseType) getContext().getType(clazz);
+      } catch (ClassNotFoundException e) {
+        LOG.debug("Class {} not found.", qualifiedName, e);
         return null;
+      } catch (NoClassDefFoundError e) {
+        LOG.debug("Failed to find a dependency.", e);
+        return null;
+      }
     }
 
-    private static CodeElementWithTypeVariables findElementWithTypeVariables(CodeNode node) {
+    @Override
+    public BaseType getType(CodeName qualifiedName) {
 
-        if (node instanceof CodeElementWithTypeVariables) {
-            return (CodeElementWithTypeVariables) node;
-        } else if (node instanceof BaseOperationArg) {
-            BaseOperationArg arg = (BaseOperationArg) node;
-            BaseOperation operation = arg.getDeclaringOperation();
-            if (operation != null) {
-                return operation;
-            } else {
-                return arg.getDeclaringType();
-            }
-        } else if (node instanceof BaseGenericType) {
-            return findElementWithTypeVariables(node.getParent());
-        } else if (node instanceof BaseTypeVariables) {
-            return findElementWithTypeVariables(node.getParent());
+      return getType(qualifiedName.getFullName());
+    }
+
+    @Override
+    public BaseGenericType getType(Class<?> clazz) {
+
+      if (clazz.isArray()) {
+        BaseGenericType componentType = getType(clazz.getComponentType());
+        return componentType.createArray();
+      }
+      CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
+      BaseSource source = getOrCreateSource(codeSource);
+      JavaSourceLoader javaSourceLoader = (JavaSourceLoader) source.getLoader();
+
+      // HERE
+      if (codeSource != source.getReflectiveObject()) {
+        BasePackage parentPackage;
+        Package pkg = clazz.getPackage();
+        if (pkg == null) {
+          parentPackage = getSource().getRootPackage();
         } else {
-            throw new IllegalCaseException(node.getClass().getSimpleName());
+          String pkgName = pkg.getName();
+          parentPackage = javaSourceLoader.getPackage(source.parseName(pkgName));
         }
+        return javaSourceLoader.getTypeInternal(clazz, parentPackage);
+      }
+      return source.getLoader().getType(clazz);
     }
 
     /**
-     * Implementation of {@link BaseLoader} to load classes from byte-code.
-     *
-     * @see JavaContext#getLoader()
+     * Gets the current class loader
+     * 
+     * @return class loader
      */
-    protected class JavaClassLoader implements BaseLoader {
+    @Override
+    public ClassLoader getClassLoader() {
 
-        private final ClassLoader classloader;
-
-        /**
-         * The constructor.
-         */
-        public JavaClassLoader() {
-
-            this(Thread.currentThread().getContextClassLoader());
-        }
-
-        /**
-         * The constructor.
-         *
-         * @param classloader
-         *            the explicit {@link ClassLoader} to use.
-         */
-        public JavaClassLoader(ClassLoader classloader) {
-
-            super();
-            this.classloader = classloader;
-        }
-
-        @Override
-        public BaseSource getSource() {
-
-            return JavaContext.this.getSource();
-        }
-
-        @Override
-        public BaseContext getContext() {
-
-            return JavaContext.this;
-        }
-
-        @Override
-        public BaseType getType(String qualifiedName) {
-            Class<?> clazz = null;
-            try {
-                clazz = classloader.loadClass(qualifiedName);
-                if (clazz.isArray()) {
-                    throw new IllegalArgumentException(qualifiedName);
-                }
-                return (BaseType) getContext().getType(clazz);
-            } catch (ClassNotFoundException e) {
-                LOG.debug("Class {} not found.", qualifiedName, e);
-                return null;
-            } catch (NoClassDefFoundError e) {
-                LOG.debug("Failed to find a dependency.", e);
-                return null;
-            }
-        }
-
-        @Override
-        public BaseType getType(CodeName qualifiedName) {
-
-            return getType(qualifiedName.getFullName());
-        }
-
-        @Override
-        public BaseGenericType getType(Class<?> clazz) {
-
-            if (clazz.isArray()) {
-                BaseGenericType componentType = getType(clazz.getComponentType());
-                return componentType.createArray();
-            }
-            CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
-            BaseSource source = getOrCreateSource(codeSource);
-            JavaSourceLoader javaSourceLoader = (JavaSourceLoader) source.getLoader();
-
-            // HERE
-            if (codeSource != source.getReflectiveObject()) {
-                BasePackage parentPackage;
-                Package pkg = clazz.getPackage();
-                if (pkg == null) {
-                    parentPackage = getSource().getRootPackage();
-                } else {
-                    String pkgName = pkg.getName();
-                    parentPackage = javaSourceLoader.getPackage(source.parseName(pkgName));
-                }
-                return javaSourceLoader.getTypeInternal(clazz, parentPackage);
-            }
-            return source.getLoader().getType(clazz);
-        }
-
-        /**
-         * Gets the current class loader
-         * @return class loader
-         */
-        @Override
-        public ClassLoader getClassLoader() {
-            return classloader;
-        }
-
+      return classloader;
     }
+
+  }
 
 }
